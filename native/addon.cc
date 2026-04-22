f7729e0ef740d94808637e1c3d3252344488068a
jinpy.he <jinpy.he@kuehne-nagel.com>
2026-04-22 10:32:05 +0800

    Fix Windows header includes for MSVC

--- native/addon.cc
+++ native/addon.cc
@@ -1,6 +1,13 @@
 #include <napi.h>
 
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <windows.h>
 #include <wincrypt.h>
 #include <winldap.h>
 #else
